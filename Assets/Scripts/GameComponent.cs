ce7a08202dfad0cf1aaacacbe7be1e994ef8caca
Mikko Romppainen <mikko.romppainen@kamk.fi>
2020-01-31 20:29:45 +0200

    Lisätty GameComponent and GameManager.

---
+++ Assets/Scripts/GameComponent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Use this class as a base class for each component
+/// </summary>
+public class GameComponent : MonoBehaviour
+{
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Main game controller.
+/// </summary>
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
