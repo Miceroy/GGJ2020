2b734d9f7243e22f7c1936aa6e10cd1ba0a1a75f
Mikko Romppainen <mikko.romppainen@kamk.fi>
2020-02-01 16:01:48 +0200

    Spawn enemies.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,21 @@ using System.Collections;
 /// </summary>
 public class GameManager : MonoBehaviour
 {
+    private Vector3 getRandomSpawnPoint()
+    {
+        Debug.AssertFormat(GameObject.FindGameObjectWithTag("PlayerBoat"), "PlayerBoat not found");
+        Vector3 res = GameObject.FindGameObjectWithTag("PlayerBoat").transform.position;
+        float angle = Random.Range(0, 2.0f*Mathf.PI);
+        float x = Mathf.Cos(angle) * spawnRadius;
+        float y = Mathf.Sin(angle) * spawnRadius;
+        res.x += x;
+        res.y = 1;
+        res.z += y;
+        return res;
+    }
+
+    public GameObject enemyBoatPrefab;
+    public float spawnRadius = 100;
     public List<GameComponent> gameComponents = new List<GameComponent>();
 
     public void registerComponent(GameComponent newComponent)
@@ -20,15 +35,22 @@ public class GameManager : MonoBehaviour
         gameComponents.Remove(componentToRemove);
     }
 
+    public void spawnNewEnemy()
+    {
+        GameObject enemyBoat = GameObject.Instantiate(enemyBoatPrefab, getRandomSpawnPoint(),Quaternion.identity);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnNewEnemy();
+        spawnNewEnemy();
+        spawnNewEnemy();
+        spawnNewEnemy();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
