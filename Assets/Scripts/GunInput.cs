1570722cd408065e6131a563598142748fba0df7
AJ-Ora <antti-jussioravainen@kamk.fi>
2020-02-01 20:36:06 +0200

    Improved GunInput, now guns shoot properly.

--- Assets/Scripts/GunInput.cs
+++ Assets/Scripts/GunInput.cs
@@ -1,18 +1,48 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿//using System.Collections;
+//using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR;
+using Valve.VR.InteractionSystem;
 
 public class GunInput : MonoBehaviour
 {
     public SteamVR_Action_Boolean shoot;
-    public SteamVR_Input_Sources handType;
     public GunBehaviour behaviour;
     public Animator gunAnimator;
 
+    private SteamVR_Input_Sources hand;
+    private Interactable interactable;
+    private bool isActive = false;
+
     private void Start()
     {
-        shoot.AddOnStateDownListener(OnTriggerPress, handType);
+        interactable = GetComponent<Interactable>();
+        if (interactable == null)
+        {
+            Debug.LogWarning(this + " Script couldn't find Interactable component!");
+            this.enabled = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (interactable.attachedToHand)
+        {
+            if (!isActive)
+            {
+                hand = interactable.attachedToHand.handType;
+                shoot.AddOnStateDownListener(OnTriggerPress, hand);
+                isActive = !isActive;
+            }
+        }
+        else
+        {
+            if (isActive)
+            {
+                shoot.RemoveOnStateDownListener(OnTriggerPress, hand);
+                isActive = !isActive;
+            }
+        }
     }
 
     private void OnTriggerPress(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
@@ -20,7 +50,6 @@ public class GunInput : MonoBehaviour
         if (behaviour != null)
         {
             behaviour.Shoot();
-
             if (gunAnimator != null)
             {
                 gunAnimator.SetTrigger("Shoot");
