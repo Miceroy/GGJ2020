9e2f94e21117e787f8e57a3c51b2c5ae10222cf3
TommiHelin <>
2020-02-01 21:46:24 +0200

    Merge

---
+++ Assets/Scripts/GunCockingAudio.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+
+public class GunCockingAudio : MonoBehaviour
+{
+    public LinearMapping linear;
+    public AudioSource source;
+    public AudioClip pullSound;
+    public AudioClip releaseSound;
+
+    private bool fullyPulled = false;
+    
+    void Update()
+    {
+        if (linear != null)
+        {
+            if (linear.value > 0.9f && !fullyPulled)
+            {
+                if (source != null)
+                {
+                    source.Stop();
+                    source.clip = pullSound;
+                    source.Play();
+                }
+                fullyPulled = !fullyPulled;
+            }
+            else if (linear.value < 0.1f && fullyPulled)
+            {
+                if (source != null)
+                {
+                    source.Stop();
+                    source.clip = releaseSound;
+                    source.Play();
+                }
+                fullyPulled = !fullyPulled;
+            }
+        }
+    }
+}
