6eff648ebac1264368247a5d2fb57026cd58c919
AJ-Ora <antti-jussioravainen@kamk.fi>
2020-02-01 19:51:55 +0200

    Made progress on implementing shooting.

---
+++ Assets/Scripts/GunInput.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class GunInput : MonoBehaviour
+{
+    public SteamVR_Action_Boolean shoot;
+    public SteamVR_Input_Sources handType;
+    public GunBehaviour behaviour;
+    public Animator gunAnimator;
+
+    private void Start()
+    {
+        shoot.AddOnStateDownListener(OnTriggerPress, handType);
+    }
+
+    private void OnTriggerPress(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        if (behaviour != null)
+        {
+            behaviour.Shoot();
+
+            if (gunAnimator != null)
+            {
+                gunAnimator.SetTrigger("Shoot");
+            }
+        }
+        else
+        {
+            Debug.LogWarning(this + " Component is missing GunBehaviour script!");
+        }
+    }
+}
