96ae2dfc4eeda0c2efa80a2f055322b7e996305e
Kupiainen Kim TTK18SP <kimkupiainen@kamk.fi>
2020-02-01 12:18:45 +0200

    pistolanimationfix

---
+++ Assets/Scripts/PlayParticleOnAwake.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayParticleOnAwake : MonoBehaviour
+{
+    private void OnAwake()
+    {
+        foreach(ParticleSystem particleSystem in transform.GetComponents<ParticleSystem>())
+        {
+            particleSystem.Play();
+        }
+    }
+}
